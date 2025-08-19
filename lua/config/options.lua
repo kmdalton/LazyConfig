3d781167b2a7f94c06ee3aae769f083b2182b9ff
Kevin Dalton <kmdalton@slac.stanford.edu>
2025-08-19 17:38:05 -0400

    override some lazy default options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- LazyVim auto format
+vim.g.autoformat = false
+
+-- Snacks animations
+-- Set to `false` to globally disable all snacks animations
+vim.g.snacks_animate = false
+
+vim.opt.relativenumber = false -- Relative line numbers
+vim.opt.shiftwidth = 4 -- Size of an indent
+
